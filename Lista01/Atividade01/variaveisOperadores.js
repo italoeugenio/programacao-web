90c1fc1ea42b652a3e748bd2c6f8f0606ba2ce2b
ÍTALO CEZAR EUGÊNIO DE SANTANA <italo.santana@a.ucb.br>
2024-03-13 10:06:29 -0300

    add atividade 01

---
+++ Lista01/Atividade01/variaveisOperadores.js
@@ -0,0 +1,9 @@
+var num1 = 20;
+var num2 = 5;
+
+var div =  num1 / num2;
+
+console.log(`A soma entre ${num1} e ${num2} é ${num1 + num2}`);
+console.log(`A subtração entre ${num1} e ${num2} é ${num1 - num2}`);
+console.log(`A multiplicação entre ${num1} e ${num2} é ${num1 * num2}`);
+console.log(`A divisão entre ${num1} e ${num2} é ${div}`);
