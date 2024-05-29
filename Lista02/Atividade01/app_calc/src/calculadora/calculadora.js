4603639723ff6f00f227122a843f1c5f0f2f6eb0
Ítalo Santana <italoeugenio539@gmail.com>
2024-05-28 21:25:10 -0300

    corrigindo erros

--- Lista02/Atividade01/app_calc/src/calculadora/calculadora.js
+++ Lista02/Atividade01/app_calc/src/calculadora/calculadora.js
@@ -1,30 +1,15 @@
-exports.calcular = function(num1, num2, operacao) {
-
-  if (!Number.isFinite(num1) ||!Number.isFinite(num2)) {
-      throw new Error('Os valores devem ser números.');
-  }
-
+exports.calcular = (num1, num2, operacao) => {
   let resultado;
-  switch (operacao.toLowerCase()) { 
-      case 'soma':
-          resultado = num1 + num2;
-          break;
-      case 'subtracao':
-          resultado = num1 - num2;
-          break;
-      case 'multiplicacao':
-          resultado = num1 * num2;
-          break;
-      case 'divisao':
-          if (num2!== 0) {
-              resultado = num1 / num2;
-          } else {
-              throw new Error('Divisão por zero não é permitida.');
-          }
-          break;
-      default:
-          throw new Error('Operação inválida');
+  if (operacao === '+') {
+    resultado = num1 + num2;
+  } else if (operacao === '-') {
+    resultado = num1 - num2;
+  } else if (operacao === '*') {
+    resultado = num1 * num2;
+  } else if (operacao === '/') {
+    resultado = num1 / num2;
+  } else {
+    throw new Error('ERRO: OPERAÇÃO INVÁLIDA');
   }
-  
   return resultado;
 };
--- Lista02/Atividade01/app_calc/src/controller/calculadoraController.js
+++ Lista02/Atividade01/app_calc/src/controller/calculadoraController.js
@@ -1,19 +1,38 @@
 const calculadora = require('../calculadora/calculadora');
 
+function validarNumeros(numero1, numero2) {
+  if (isNaN(numero1) || isNaN(numero2)) {
+    return 'Insira valores válidos';
+  }
+  return true;
+}
+
+function calcularResultado(numero1, numero2, operacao) {
+  return calculadora.calcular(Number(numero1), Number(numero2), operacao);
+}
+
+function prepararDadosParaRenderizacao(numero1, numero2, resultado) {
+  return {
+    primeiroNumero: numero1,
+    segundoNumero: numero2,
+    result: resultado
+  };
+}
+
 exports.renderIndex = (req, res) => {
-    res.render('index.html');
+  res.render('index.html');
 };
 
-exports.calcular = async (req, res) => {
-    try {
-        const { primeiroNumero, segundoNumero, operacao } = req.body;
-        if (isNaN(primeiroNumero) || isNaN(segundoNumero)) {
-            return res.render('index.html', { errorMessage: 'Você inseriu valores invalidos' });
-        }
-        const resultado = await calculadora.calcular(parseFloat(primeiroNumero), parseFloat(segundoNumero), operacao);
-        return res.render('resultado.html', { primeiroNumero, segundoNumero, result: resultado });
-    } catch (error) {
-        console.error(error.message);
-        return res.render('index.html', { errorMessage: 'Ocorreu algum erro.' });
-    }
+exports.calcular = (req, res) => {
+  const { primeiroNumero, segundoNumero, operacao } = req.body;
+
+  const validacao = validarNumeros(primeiroNumero, segundoNumero);
+  if (validacao !== true) {
+    res.render('index.html', { errorMessage: validacao });
+    return;
+  }
+
+  const resultado = calcularResultado(primeiroNumero, segundoNumero, operacao);
+  const dados = prepararDadosParaRenderizacao(primeiroNumero, segundoNumero, resultado);
+  res.render('resultado.html', dados);
 };
